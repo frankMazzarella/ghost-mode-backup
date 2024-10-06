53775f3a071d3cc61deabe3171f7cb38ca2c687b
Frank Mazzarella <mazzarella.frank@gmail.com>
2024-10-06 14:04:09 -0400

    running every 5 minutes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import { cp } from 'node:fs/promises'
 import { basename } from 'node:path';
+import { homedir } from 'node:os';
 
-const sourceDir = "c:/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/savegames/afd78794-7f97-479e-ae6d-ec41638fd15a";
-const backupDir = "c:/Users/Frank Mazzarella/Desktop/ghost-mode-backups";
+const SOURCE_DIR = "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames\\afd78794-7f97-479e-ae6d-ec41638fd15a";
+const BACKUP_DIR = `${homedir}\\Desktop\\ghost-mode-backups`;
+const FIVE_MINUTES_MS = 5 * 60 * 1000;
 
 const getDestinationDir = () => {
   const now = new Date();
-  return `${backupDir}/${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}T${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}/${basename(sourceDir)}`
+  return `${BACKUP_DIR}\\${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}T${now.getHours()}-${now.getMinutes()}-${now.getSeconds().toString().padStart(2, 0)}\\${basename(SOURCE_DIR)}`
 }
 
-try {
-  await cp(sourceDir, getDestinationDir(), { recursive: true });
-} catch (error) {
-  console.error(error);
+const copySaveGame = async () => {
+  try {
+    const destinationDir = getDestinationDir();
+    await cp(SOURCE_DIR, destinationDir, { recursive: true });
+    console.log(`FILES SUCCESSFULLY COPIED\n\tFROM: ${SOURCE_DIR}\n\t  TO: ${destinationDir}`)
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+copySaveGame();
+setInterval(copySaveGame, FIVE_MINUTES_MS)
