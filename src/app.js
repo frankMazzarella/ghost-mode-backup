0f4094cbe7385df4a29c89ad94fc931032c4d1b6
Frank Mazzarella <mazzarella.frank@gmail.com>
2024-10-06 19:14:16 -0400

    prettier and change dest dir

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
-import { cp } from 'node:fs/promises'
-import { basename } from 'node:path';
-import { homedir } from 'node:os';
+import { cp } from "node:fs/promises";
+import { basename } from "node:path";
 
-const SOURCE_DIR = "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames\\afd78794-7f97-479e-ae6d-ec41638fd15a";
-const BACKUP_DIR = `${homedir}\\Desktop\\ghost-mode-backups`;
+const SOURCE_DIR =
+  "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames\\afd78794-7f97-479e-ae6d-ec41638fd15a";
 const FIVE_MINUTES_MS = 5 * 60 * 1000;
+
+const initialize = () => {
+  // check if config.json exists
+  // if not create with default data
+  // prompt to fill in
+  // check if game save location exists
+  // prompt to fix data
+};
 
 const getDestinationDir = () => {
   const now = new Date();
@@ -13,18 +20,21 @@ const getDestinationDir = () => {
   const minutes = `${now.getMinutes().toString().padStart(2, 0)}`;
   const seconds = `${now.getSeconds().toString().padStart(2, 0)}`;
   const timeStr = `${hours}-${minutes}-${seconds}`;
-  return `${BACKUP_DIR}\\${dateStr}T${timeStr}\\${basename(SOURCE_DIR)}`;
-}
+  return `savegames\\${dateStr}T${timeStr}\\${basename(SOURCE_DIR)}`;
+};
 
 const copySaveGame = async () => {
   try {
     const destinationDir = getDestinationDir();
     await cp(SOURCE_DIR, destinationDir, { recursive: true });
-    console.log(`FILES SUCCESSFULLY COPIED\n\tFROM: ${SOURCE_DIR}\n\t  TO: ${destinationDir}`)
+    console.log(
+      `FILES SUCCESSFULLY COPIED\n\tFROM: ${SOURCE_DIR}\n\t  TO: ${destinationDir}`
+    );
   } catch (error) {
     console.error(error);
   }
-}
+};
 
+initialize();
 copySaveGame();
-setInterval(copySaveGame, FIVE_MINUTES_MS)
+setInterval(copySaveGame, FIVE_MINUTES_MS);
