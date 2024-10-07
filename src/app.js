51d286e1f45d2c6839f788a3ecbdce255553f73f
Frank Mazzarella <mazzarella.frank@gmail.com>
2024-10-06 21:22:44 -0400

    working from config file

--- src/app.js
+++ src/app.js
@@ -1,16 +1,82 @@
-import { cp } from "node:fs/promises";
+import { cp, stat, writeFile, readFile } from "node:fs/promises";
 import { basename } from "node:path";
 
-const SOURCE_DIR =
-  "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames\\afd78794-7f97-479e-ae6d-ec41638fd15a";
-const FIVE_MINUTES_MS = 5 * 60 * 1000;
+let config;
 
-const initialize = () => {
-  // check if config.json exists
-  // if not create with default data
-  // prompt to fill in
-  // check if game save location exists
-  // prompt to fix data
+// TODO: should probably be using path.join
+
+const defaultConfigData = {
+  backupIntervalMinutes: 15,
+  sourceDir:
+    "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames",
+  gameId: "",
+};
+
+const initialize = async () => {
+  const configExists = await getConfigExists();
+  if (configExists) {
+    await loadConfigData();
+    const isConfigDataValid = await getConfigDataValid();
+    if (isConfigDataValid) {
+      console.log(
+        `starting savegame backup at ${config.backupIntervalMinutes} minute intervals`
+      );
+      copySaveGame();
+      setInterval(copySaveGame, config.backupIntervalMinutes * 60 * 1000);
+    }
+  } else {
+    await createConfigFile();
+    console.warn(
+      "make sure to edit config.json and add the correct wildlands game id"
+    );
+  }
+};
+
+const getConfigDataValid = async () => {
+  if (!config.gameId) {
+    console.error(
+      "you must edit config.json and set the wildlands game id value"
+    );
+    return false;
+  }
+  try {
+    const data = await stat(getSourceDir());
+  } catch {
+    console.error("wildlands game id value is not correct");
+    return false;
+  }
+  return true;
+};
+
+const loadConfigData = async () => {
+  try {
+    config = JSON.parse(await readFile("config.json"));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getConfigExists = async () => {
+  try {
+    await stat("config.json");
+    return true;
+  } catch {
+    console.log("config.json file does not exist");
+    return false;
+  }
+};
+
+const createConfigFile = async () => {
+  try {
+    await writeFile("config.json", JSON.stringify(defaultConfigData, null, 2));
+    console.log("config.json file has been created with default data");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getSourceDir = () => {
+  return `${config.sourceDir}\\${config.gameId}`;
 };
 
 const getDestinationDir = () => {
@@ -20,15 +86,16 @@ const getDestinationDir = () => {
   const minutes = `${now.getMinutes().toString().padStart(2, 0)}`;
   const seconds = `${now.getSeconds().toString().padStart(2, 0)}`;
   const timeStr = `${hours}-${minutes}-${seconds}`;
-  return `savegames\\${dateStr}T${timeStr}\\${basename(SOURCE_DIR)}`;
+  return `backups\\${dateStr}T${timeStr}\\${basename(getSourceDir())}`;
 };
 
 const copySaveGame = async () => {
   try {
+    const sourceDir = getSourceDir();
     const destinationDir = getDestinationDir();
-    await cp(SOURCE_DIR, destinationDir, { recursive: true });
+    await cp(sourceDir, destinationDir, { recursive: true });
     console.log(
-      `FILES SUCCESSFULLY COPIED\n\tFROM: ${SOURCE_DIR}\n\t  TO: ${destinationDir}`
+      `FILES SUCCESSFULLY COPIED\n\tFROM: ${sourceDir}\n\t  TO: ${destinationDir}`
     );
   } catch (error) {
     console.error(error);
@@ -36,5 +103,3 @@ const copySaveGame = async () => {
 };
 
 initialize();
-copySaveGame();
-setInterval(copySaveGame, FIVE_MINUTES_MS);
